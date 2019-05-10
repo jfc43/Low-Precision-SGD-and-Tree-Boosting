391e5caa92c57de6f1702f8066df7c340401fe01
yw <yw@node-0.low-pr-xgboost.cs744-s19-pg0.wisc.cloudlab.us>
2019-05-10 18:41:55 -0500

    Add XGBoost

---
+++ XGBoost/dmlc-core/src/io/filesys.cc
@@ -0,0 +1,58 @@
+// Copyright by Contributors
+
+#include <dmlc/filesystem.h>
+#include <queue>
+
+namespace dmlc {
+namespace io {
+
+void FileSystem::ListDirectoryRecursive(const URI &path,
+                                        std::vector<FileInfo> *out_list) {
+  std::queue<URI> queue;
+  queue.push(path);
+  while (!queue.empty()) {
+    std::vector<FileInfo> dfiles;
+    ListDirectory(queue.front(), &dfiles);
+    queue.pop();
+    for (auto dfile : dfiles) {
+      if (dfile.type == kDirectory) {
+        queue.push(dfile.path);
+      } else {
+        out_list->push_back(dfile);
+      }
+    }
+  }
+}
+
+}  // namespace io
+
+void TemporaryDirectory::RecursiveDelete(const std::string &path) {
+  io::URI uri(path.c_str());
+  io::FileSystem* fs = io::FileSystem::GetInstance(uri);
+  std::vector<io::FileInfo> file_list;
+  fs->ListDirectory(uri, &file_list);
+  for (io::FileInfo info : file_list) {
+    CHECK(!IsSymlink(info.path.name))
+        << "Symlink not supported in TemporaryDirectory";
+    if (info.type == io::FileType::kDirectory) {
+      RecursiveDelete(info.path.name);
+    } else {
+      CHECK_EQ(std::remove(info.path.name.c_str()), 0)
+          << "Couldn't remove file " << info.path.name;
+    }
+  }
+#if _WIN32
+  const bool rmdir_success = (RemoveDirectoryA(path.c_str()) != 0);
+#else
+  const bool rmdir_success = (rmdir(path.c_str()) == 0);
+#endif
+  if (rmdir_success) {
+    if (verbose_) {
+      LOG(INFO) << "Successfully deleted temporary directory " << path;
+    }
+  } else {
+    LOG(FATAL) << "~TemporaryDirectory(): "
+               << "Could not remove temporary directory " << path;
+  }
+}
+}  // namespace dmlc
